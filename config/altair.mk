65fcca99b397544e998c0b1757f7e51d32949381
Martin J. Pollard <martin0566@gmail.com>
2021-09-11 11:13:44 -0400

    config: Enable window blur on most devices
    But also allow devices to opt out from blur by setting
    
        TARGET_DISABLE_BLUR := true
    
    in device tree's device.mk.
    
    (Adapted from similar modifications in crDroid)

--- config/altair.mk
+++ config/altair.mk
@@ -4,3 +4,10 @@ PRODUCT_PACKAGES += \
     LineageDarkGrayBackground \
     LineageMatteBlackBackground \
     LineageSolarizedBackground
+
+# Blur
+ifneq ($(TARGET_DISABLE_BLUR), true)
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.sf.blurs_are_expensive=1 \
+    ro.surface_flinger.supports_background_blur=1
+endif
