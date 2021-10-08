bd5e1684905545113083db264c9607569e24da5f
Martin J. Pollard <martin0566@gmail.com>
2021-10-07 22:19:50 -0400

    lineage: Link vendor/addons

--- config/altair.mk
+++ config/altair.mk
@@ -11,3 +11,11 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.sf.blurs_are_expensive=1 \
     ro.surface_flinger.supports_background_blur=1
 endif
+
+# Fonts
+PRODUCT_PACKAGES += \
+    fonts_customization.xml \
+    ComicSansFont \
+    GoogleSansFont \
+    LineageLatoFont \
+    LineageRubikFont
