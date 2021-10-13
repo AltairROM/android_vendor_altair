4eb8d2ac4a6b2531a48a12c02d4dbce709e95305
Martin J. Pollard <martin0566@gmail.com>
2021-10-12 20:36:23 -0400

    lineage: Move font package inclusion to vendor/addons

--- config/altair.mk
+++ config/altair.mk
@@ -11,11 +11,3 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.sf.blurs_are_expensive=1 \
     ro.surface_flinger.supports_background_blur=1
 endif
-
-# Fonts
-PRODUCT_PACKAGES += \
-    fonts_customization.xml \
-    ComicSansFont \
-    GoogleSansFont \
-    LineageLatoFont \
-    LineageRubikFont
