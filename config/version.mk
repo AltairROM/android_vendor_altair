470d575823d34a7d6052fe61ed19de59bdf6cf19
Martin J. Pollard <martin0566@gmail.com>
2022-05-13 21:12:51 -0400

    config: Use local time instead of UTC for LINEAGE_BUILD_DATE

--- config/version.mk
+++ config/version.mk
@@ -1,7 +1,7 @@
 PRODUCT_VERSION_MAJOR = 16
 PRODUCT_VERSION_MINOR = 2
 
-LINEAGE_BUILD_DATE := $(shell date -u +%Y%m%d)
+LINEAGE_BUILD_DATE := $(shell date +%Y%m%d)
 
 # Set LINEAGE_BUILDTYPE from the env RELEASE_TYPE, for jenkins compat
 
