8f0cae0252f279d5bcab8bb891cbb6d36b835fa4
Martin J. Pollard <martin0566@gmail.com>
2022-11-11 14:07:51 -0500

    Initial commit for Altair ROM 16
    - LineageOS is the base ROM, but there are/will be additions over it.
      Therefore, rebrand and release it as Altair ROM.

--- build/tools/roomservice.py
+++ build/tools/roomservice.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # Copyright (C) 2012-2013, The CyanogenMod Project
 #           (C) 2017-2018,2020-2021, The LineageOS Project
+#           (C) 2024, The Evolution X Project
+#           (C) 2025, The Altair ROM Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,11 +18,12 @@
 
 from __future__ import print_function
 
+import base64
 import glob
 import json
+import netrc
 import os
 import re
-import subprocess
 import sys
 import urllib.error
 import urllib.parse
@@ -45,22 +48,38 @@ except:
     device = product
 
 if not depsonly:
-    print("Device %s not found. Attempting to retrieve device repository from LineageOS Github (http://github.com/LineageOS)." % device)
+    print("Device %s not found. Attempting to retrieve device repository from AltairROM-Devices Github (http://github.com/AltairROM-Devices)." % device)
 
 repositories = []
 
+try:
+    authtuple = netrc.netrc().authenticators("api.github.com")
+
+    if authtuple:
+        auth_string = ('%s:%s' % (authtuple[0], authtuple[2])).encode()
+        githubauth = base64.encodestring(auth_string).decode().replace('\n', '')
+    else:
+        githubauth = None
+except:
+    githubauth = None
+
+def add_auth(githubreq):
+    if githubauth:
+        githubreq.add_header("Authorization","Basic %s" % githubauth)
+
 if not depsonly:
-    githubreq = urllib.request.Request("https://raw.githubusercontent.com/LineageOS/mirror/main/default.xml")
+    githubreq = urllib.request.Request("https://api.github.com/search/repositories?q=%s+user:AltairROM-Devices+in:name+fork:true" % device)
+    add_auth(githubreq)
     try:
-        result = ElementTree.fromstring(urllib.request.urlopen(githubreq, timeout=10).read().decode())
+        result = json.loads(urllib.request.urlopen(githubreq, timeout=10).read().decode())
     except urllib.error.URLError:
-        print("Failed to fetch data from GitHub")
+        print("Failed to search GitHub")
         sys.exit(1)
     except ValueError:
         print("Failed to parse return data from GitHub")
         sys.exit(1)
-    for res in result.findall('.//project'):
-        repositories.append(res.attrib['name'][10:])
+    for res in result.get('items', []):
+        repositories.append(res)
 
 local_manifests = r'.repo/local_manifests'
 if not os.path.exists(local_manifests): os.makedirs(local_manifests)
@@ -100,12 +119,6 @@ def get_manifest_path():
     except IndexError:
         return ".repo/manifests/{}".format(m.find("include").get("name"))
 
-def get_default_revision():
-    m = ElementTree.parse(get_manifest_path())
-    d = m.findall('default')[0]
-    r = d.get('revision')
-    return r.replace('refs/heads/', '').replace('refs/tags/', '')
-
 def get_from_manifest(devicename):
     for path in glob.glob(".repo/local_manifests/*.xml"):
         try:
@@ -115,7 +128,7 @@ def get_from_manifest(devicename):
             lm = ElementTree.Element("manifest")
 
         for localpath in lm.findall("project"):
-            if re.search("android_device_.*_%s$" % device, localpath.get("name")):
+            if re.search("device_.*_%s$" % device, localpath.get("name")):
                 return localpath.get("path")
 
     return None
@@ -154,6 +167,17 @@ def is_in_manifest(projectpath):
         if localpath.get("path") == projectpath:
             return True
 
+    # ... and don't forget the altair snippet
+    try:
+        lm = ElementTree.parse(".repo/manifests/snippets/altair.xml")
+        lm = lm.getroot()
+    except:
+        lm = ElementTree.Element("manifest")
+
+    for localpath in lm.findall("project"):
+        if localpath.get("path") == projectpath:
+            return True
+
     return False
 
 def add_to_manifest(repositories):
@@ -169,40 +193,43 @@ def add_to_manifest(repositories):
     for repository in repositories:
         repo_name = repository['repository']
         repo_target = repository['target_path']
-        repo_revision = repository['branch']
+        repo_remote = repository.get("remote", "altair-devices")
+        repo_revision = repository.get('revision')
+
+        if repo_remote != "altair-devices" and not repo_remote.startswith("aosp-"):
+            repo_revision = repo_revision or repository.get('branch') or get_default_or_fallback_revision(repo_name)
+
         print('Checking if %s is fetched from %s' % (repo_target, repo_name))
         if is_in_manifest(repo_target):
-            print('LineageOS/%s already fetched to %s' % (repo_name, repo_target))
+            print('%s already fetched to %s' % (repo_name, repo_target))
             continue
 
-        project = ElementTree.Element("project", attrib = {
+        project_attrib = {
             "path": repo_target,
-            "remote": "github",
-            "name": "LineageOS/%s" % repo_name,
-            "revision": repo_revision })
-        if repo_remote := repository.get("remote", None):
-            # aosp- remotes are only used for kernel prebuilts, thus they
-            # don't let you customize clone-depth/revision.
-            if repo_remote.startswith("aosp-"):
-                project.attrib["name"] = repo_name
-                project.attrib["remote"] = repo_remote
-                project.attrib["clone-depth"] = "1"
-                del project.attrib["revision"]
-        if project.attrib.get("revision", None) == get_default_revision():
-            del project.attrib["revision"]
-        print("Adding dependency: %s -> %s" % (project.attrib["name"], project.attrib["path"]))
+            "name": repo_name,
+            "remote": repo_remote,
+        }
+
+        if repo_revision and not (repo_remote == "altair-devices" and repo_revision == get_default_or_fallback_revision(repo_name)):
+            project_attrib["revision"] = repo_revision
+
+        project = ElementTree.Element("project", attrib=project_attrib)
+
+        if repo_remote.startswith("aosp-"):
+            project.set("clone-depth", "1")
+            project.attrib.pop('revision', None)
+
+        print("Adding dependency: %s -> %s" % (repo_name, repo_target))
         lm.append(project)
 
     indent(lm, 0)
     raw_xml = ElementTree.tostring(lm).decode()
     raw_xml = '<?xml version="1.0" encoding="UTF-8"?>\n' + raw_xml
 
-    f = open('.repo/local_manifests/roomservice.xml', 'w')
-    f.write(raw_xml)
-    f.close()
+    with open('.repo/local_manifests/roomservice.xml', 'w') as f:
+        f.write(raw_xml)
 
 def fetch_dependencies(repo_path):
-    print('Looking for dependencies in %s' % repo_path)
     dependencies_path = repo_path + '/lineage.dependencies'
     syncable_repos = []
     verify_repos = []
@@ -233,48 +260,43 @@ def fetch_dependencies(repo_path):
         if len(fetch_list) > 0:
             print('Adding dependencies to manifest')
             add_to_manifest(fetch_list)
-    else:
-        print('%s has no additional dependencies.' % repo_path)
 
     if len(syncable_repos) > 0:
         print('Syncing dependencies')
         if not dryrun:
-            os.system('repo sync --force-sync %s' % ' '.join(syncable_repos))
+            os.system('repo sync --force-sync --no-tags --current-branch --no-clone-bundle -j2 %s' % ' '.join(syncable_repos))
 
     for deprepo in verify_repos:
         fetch_dependencies(deprepo)
 
-def get_default_or_fallback_revision(repo_name):
-    default_revision = get_default_revision()
-    print("Default revision: %s" % default_revision)
-    print("Checking branch info")
-
+def get_remote_revision_from_snippet(snippet_path, remote_name):
     try:
-        stdout = subprocess.run(
-            ["git", "ls-remote", "-h", "https://:@github.com/LineageOS/" + repo_name],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-        ).stdout.decode()
-        branches = [x.split("refs/heads/")[-1] for x in stdout.splitlines()]
-    except:
-        return ""
+        tree = ElementTree.parse(snippet_path)
+        root = tree.getroot()
+        remote = root.find(f".//remote[@name='{remote_name}']")
+        if remote is not None:
+            revision = remote.get('revision')
+            return revision.split('/')[-1]
+    except FileNotFoundError:
+        print(f"Error: {snippet_path} not found.")
+        raise
+    except ElementTree.ParseError:
+        print(f"Error: There was a problem parsing {snippet_path}.")
+        raise
 
-    if default_revision in branches:
-        return default_revision
+    raise ValueError(f"Remote {remote_name} not found in the snippet.")
 
-    if os.getenv('ROOMSERVICE_BRANCHES'):
-        fallbacks = list(filter(bool, os.getenv('ROOMSERVICE_BRANCHES').split(' ')))
-        for fallback in fallbacks:
-            if fallback in branches:
-                print("Using fallback branch: %s" % fallback)
-                return fallback
+def get_default_or_fallback_revision(repo_name):
+    default_revision = get_remote_revision_from_snippet(".repo/manifests/snippets/altair.xml", "altair")
+    print("Using default revision: %s" % default_revision)
 
-    print("Default revision %s not found in %s. Bailing." % (default_revision, repo_name))
-    print("Branches found:")
-    for branch in branches:
-        print(branch)
-    print("Use the ROOMSERVICE_BRANCHES environment variable to specify a list of fallback branches.")
-    return ""
+    fallback_revision = os.getenv('ROOMSERVICE_FALLBACK_REVISION')
+
+    if fallback_revision:
+        print("Using fallback revision from environment: %s" % fallback_revision)
+        return fallback_revision
+
+    return default_revision
 
 if depsonly:
     repo_path = get_from_manifest(device)
@@ -286,10 +308,11 @@ if depsonly:
     sys.exit()
 
 else:
-    for repo_name in repositories:
+    for repository in repositories:
+        repo_name = repository['name']
         if re.match(r"^android_device_[^_]*_" + device + "$", repo_name):
-            print("Found repository: %s" % repo_name)
-            
+            print("Found repository: %s" % repository['name'])
+
             manufacturer = repo_name.replace("android_device_", "").replace("_" + device, "")
             repo_path = "device/%s/%s" % (manufacturer, device)
             revision = get_default_or_fallback_revision(repo_name)
@@ -304,11 +327,11 @@ else:
             add_to_manifest([device_repository])
 
             print("Syncing repository to retrieve project.")
-            os.system('repo sync --force-sync %s' % repo_path)
+            os.system('repo sync --force-sync --no-tags --current-branch --no-clone-bundle -j2 %s' % repo_path)
             print("Repository synced!")
 
             fetch_dependencies(repo_path)
             print("Done")
             sys.exit()
 
-print("Repository for %s not found in the LineageOS Github repository list. If this is in error, you may need to manually add it to your local_manifests/roomservice.xml." % device)
+print("Repository for %s not found in the AltairROM-Devices Github repository list. If this is an error, you may need to manually add it to your local_manifests/roomservice.xml." % device)
