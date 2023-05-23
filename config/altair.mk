f16f66dabd1a673e587f2f300c2dfed25f829994
Martin J. Pollard <martin0566@gmail.com>
2023-05-22 22:25:16 -0400

    config: Allow blurs if enabled for device

--- config/altair.mk
+++ config/altair.mk
@@ -1,3 +1,11 @@
+# Blurs
+ifeq ($(TARGET_ENABLE_BLUR), true)
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.sf.blurs_are_expensive=1 \
+    ro.surface_flinger.supports_background_blur=1 \
+    ro.launcher.blur.appLaunch=0
+endif
+
 # Extra packages
 PRODUCT_PACKAGES += \
     ParallelSpace
