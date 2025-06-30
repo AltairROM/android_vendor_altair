ecb5074431f6d2f748fa9f9b4b5db7aaead54d26
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-07-01 05:14:46 +0800

    altair: Update blur support
    Change-Id: Ie503986ef2642dbd474bfa5bee8d67688788d544
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/altair.mk
+++ config/altair.mk
@@ -25,15 +25,17 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.input.video_enabled=false
 
-# Blurs
-ifeq ($(TARGET_ENABLE_BLUR), true)
-PRODUCT_SYSTEM_EXT_PROPERTIES += \
-    ro.sf.blurs_are_expensive=1 \
-    ro.surface_flinger.supports_background_blur=1
+# Enable blur
+TARGET_ENABLE_BLUR ?= false
+ifeq ($(TARGET_ENABLE_BLUR),true)
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.custom.blur.enable=true
+else
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.custom.blur.enable=false
 endif
 
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.launcher.blur.appLaunch=0
+PRODUCT_SYSTEM_PROPERTIES += ro.surface_flinger.supports_background_blur=1
 
 # Disable async MTE on a few processes
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
