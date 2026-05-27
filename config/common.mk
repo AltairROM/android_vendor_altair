ae1b5c822bfb6890b0d788e7bf1f88dbca651b9e
Martin J. Pollard <martin0566@gmail.com>
2026-05-27 00:48:42 -0400

    config: Change vendor/addons to altair/addons

--- config/common.mk
+++ config/common.mk
@@ -1,7 +1,7 @@
 # Allow vendor/extra to override any property by setting it first
 $(call inherit-product-if-exists, vendor/extra/product.mk)
 $(call inherit-product-if-exists, vendor/lineage/config/altair.mk)
-$(call inherit-product-if-exists, vendor/addons/config.mk)
+$(call inherit-product-if-exists, altair/addons/config.mk)
 
 # Exclude repos from bp scanning
 PRODUCT_SOURCE_ROOT_DIRS += -kernel/platform
